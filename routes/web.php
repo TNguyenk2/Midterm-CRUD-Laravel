ff8e276ad5f0cc96c22fdf06d571a0ac2f6abf02
TNguyenk2 <nguyen.nguyen23@student.passerellesnumeriques.org>
2022-07-01 12:02:41 +0700

    ABC.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\RestaurantController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource("/restaurants", RestaurantController::class);
